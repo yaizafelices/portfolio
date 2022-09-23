5af70110c0947efb4d2c8bf539ac48c7079440df
Yaiza Soria Felices <yaizafelices@gmail.com>
2022-09-23 03:23:58 +0200

    Create header and contact

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@
 import React from "react";
 import '../styles/App.scss';
 //import PropTypes from 'prop-types';
-import {Route, Routes} from 'react-router-dom';
+//import {Route, Routes} from 'react-router-dom';
 // import { useState } from "react";
 
 
@@ -15,6 +15,9 @@ import {Route, Routes} from 'react-router-dom';
 
 import logoHeader from '../images/logo-yaiza-finalY.png';
 import '../styles/Header.scss';
+import '../styles/Home.scss';
+import '../styles/Contact.scss';
+import workInProgress from '../images/work-in-progress.png';
 
 
 function App() {
@@ -35,6 +38,46 @@ function App() {
           </nav>
 
         </header>
+        <main>
+          <section className="home">
+            <h1 className="home__hi">Hi there</h1>
+            <h2 className="home__name">I´m Yaiza a junior Front-End Developer</h2>
+            <h3 className="home__portfolio">And here you can see all about me, my personal projects and my contact</h3>
+          </section>
+          <section className="">
+            <figure className='progress'>
+              <img className='' src={workInProgress} alt="Work in progres" title='Work in progress'></img>
+            </figure>
+          </section>
+          <section className="aboutme">
+
+          </section>
+          <section className="projects">
+            
+          </section>
+          <section className="contact">
+          <nav>
+        <ul class="contact__list">
+          <li class="contact__list-icon" >
+            <a class="contact__list-icon-a"  href="www.linkedin.com/in/yaiza-soria-felices" target="_blank" rel="noreferrer"
+              ><i class="fa-brands fa-linkedin-in icon "></i
+            ></a>
+          </li>
+          <li class="contact__list-icon">
+            <a class="contact__list-icon-a"  href="https://github.com/yaizafelices" target="_blank" rel="noreferrer"
+              ><i class="fa-brands fa-github icon"></i
+            ></a>
+          </li>
+          <li class="contact__list-icon">
+            <a class="contact__list-icon-a" href="https://twitter.com/yaizafelices" target="_blank" rel="noreferrer"
+              ><i class="fa-brands fa-twitter icon"></i
+            ></a>
+          </li>
+        </ul>
+      </nav>
+            
+          </section>
+        </main>
     </div>
   );
 }
