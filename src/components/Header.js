b3c460fa1d60f020a604a2456d552631a6f358b2
Yaiza Soria Felices <yaizafelices@gmail.com>
2022-10-01 00:43:44 +0200

    Change header in mobile for a menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,18 +6,23 @@ import logoHeader from '../images/logo-yaiza-finalY.png';
 const Header = () => {
     return (
         <header className='header'>
-        <figure className='header__logo'>
-          <img className='header__logo-icon' src={logoHeader} alt="Yaiza" title='Yaiza'></img>
-        </figure>
-        <nav>
-          <ul className='header__list'>
-              <li className='header__list-li'><Link className='header__list-li-item' to='/'>Home</Link></li>
-              <li className='header__list-li'><Link className='header__list-li-item' to='/aboutme'>About Me</Link></li>
-              <li className='header__list-li'><Link className='header__list-li-item' to='/projects'>Projects</Link></li>
-              <li className='header__list-li'><Link className='header__list-li-item' to='/contact'>Contact</Link></li>
-          </ul>
-        </nav>
-      </header>
+          <nav class="header__menu">
+            
+              <i className="fas fa-bars header__menu-icon"></i>
+            
+          </nav>
+          <figure className='header__logo'>
+            <img className='header__logo-icon' src={logoHeader} alt="Yaiza" title='Yaiza'></img>
+          </figure>
+          <nav>
+            <ul className='header__list'>
+                <li className='header__list-li'><Link className='header__list-li-item' to='/'>Home</Link></li>
+                <li className='header__list-li'><Link className='header__list-li-item' to='/aboutme'>About Me</Link></li>
+                <li className='header__list-li'><Link className='header__list-li-item' to='/projects'>Projects</Link></li>
+                <li className='header__list-li'><Link className='header__list-li-item' to='/contact'>Contact</Link></li>
+            </ul>
+          </nav>
+        </header>
     );
 }
 export default Header;
