e9c5834b8b390547650c6a2079e5c15167a70795
Yaiza Soria Felices <yaizafelices@gmail.com>
2022-10-12 00:07:45 +0200

    Change position header

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,22 +8,24 @@ const Header = () => {
 
     return (
         <header className='header'>
-          <div className= "header__menu">
-            <nav className="header__menu-bars">
-                <i className="fas fa-bars header__menu-bars-icon"></i> 
-            </nav>
-          </div>
           <figure className='header__logo'>
             <img className='header__logo-icon' src={logoHeader} alt="Yaiza" title='Yaiza'></img>
           </figure>
-          <nav>
-            <ul className='header__list'>
-                <li className='header__list-li'><Link className='header__list-li-item' to='/'>Home</Link></li>
-                <li className='header__list-li'><Link className='header__list-li-item' to='/aboutme'>About Me</Link></li>
-                <li className='header__list-li'><Link className='header__list-li-item' to='/projects'>Projects</Link></li>
-                <li className='header__list-li'><Link className='header__list-li-item' to='/contact'>Contact</Link></li>
-            </ul>
-          </nav>
+          <div>
+            <div className= "header__menu">
+              <nav className="header__menu-bars">
+                  <i className="fas fa-bars header__menu-bars-icon"></i> 
+              </nav>
+            </div>
+            <nav>
+              <ul className='header__list'>
+                  <li className='header__list-li'><Link className='header__list-li-item' to='/'>Home</Link></li>
+                  <li className='header__list-li'><Link className='header__list-li-item' to='/aboutme'>About Me</Link></li>
+                  <li className='header__list-li'><Link className='header__list-li-item' to='/projects'>Projects</Link></li>
+                  <li className='header__list-li'><Link className='header__list-li-item' to='/contact'>Contact</Link></li>
+              </ul>
+            </nav>
+          </div>
         </header>
     );
 }
